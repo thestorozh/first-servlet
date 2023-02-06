53f9e9800bda55d1d8d877c3ba0a664b26e8cd1f
Andrii <40918738+thestorozh@users.noreply.github.com>
2023-02-07 00:47:56 +0200

    first

---
+++ servlet/src/main/java/HelloServlet.java
@@ -0,0 +1,50 @@
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+  
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try {
+            writer.println("<h2>Hello from HelloServlet</h2>");
+        } finally {
+            writer.close();  
+        }
+    }
+	
+  
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+          
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+ 
+        String name = request.getParameter("username");
+        String age = request.getParameter("userage");
+        String gender = request.getParameter("gender");
+        String country = request.getParameter("country");
+        String[] courses = request.getParameterValues("courses");
+         
+        try {
+            writer.println("<p>Name: " + name + "</p>");
+            writer.println("<p>Age: " + age + "</p>");
+            writer.println("<p>Gender: " + gender + "</p>");
+            writer.println("<p>Country: " + country + "</p>");
+            writer.println("<h4>Courses</h4>");
+            for(String course: courses)
+                writer.println("<li>" + course + "</li>");
+        } finally {
+            writer.close();  
+        }
+    }
+}
